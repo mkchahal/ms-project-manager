2f6133c1fc7f4ee1dedf1f1b4d5a5b7bbd202509
Mandeep <mandeep.windsor@gmail.com>
2022-08-16 19:50:36 -0700

    Added the AddClientModal component and added functionality and style to it.

---
+++ client/src/components/AddClientModal.jsx
@@ -0,0 +1,121 @@
+import { useMutation } from "@apollo/client";
+import { useState } from "react";
+import { FaUser } from "react-icons/fa";
+import { ADD_CLIENT } from "../mutations/clientMutations";
+import { GET_CLIENTS } from "../queries/clientQueries";
+
+export const AddClientModal = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const [addClient] = useMutation(ADD_CLIENT, {
+    variables: { name, email, phone },
+    update(cache, { data: { addClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+  
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: [...clients, addClient] },
+      });
+    },
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if(name === '' || email === '' || phone === '') {
+        return alert('Please enter all the fields.')
+    }
+
+    addClient(name, email, phone);
+
+    setName('');
+    setEmail('');
+    setPhone('');
+  };
+
+  return (
+    <>
+      <button
+        type="button"
+        className="btn btn-secondary"
+        data-bs-toggle="modal"
+        data-bs-target="#addClientModal"
+      >
+        <div className="d-flex align-items-center">
+          <FaUser className="icon" />
+          <div>Add New Client</div>
+        </div>
+      </button>
+
+      <div
+        className="modal fade"
+        id="addClientModal"
+        aria-labelledby="addClientModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="addClientModalLabel">
+                Add New Client
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                  <label className="form-label">Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="John Doe"
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="jdoe@email.com"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Phone</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="555-555-5555"
+                    id="phone"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
+                </div>
+
+                <button
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
