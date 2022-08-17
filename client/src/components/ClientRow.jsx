481e01f52a75d030c1be4e01c8c7d622e4e08d8f
Mandeep <mandeep.windsor@gmail.com>
2022-08-16 20:21:33 -0700

    Created Project Cards and added styling to these.

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,32 +1,48 @@
-import { FaTrash } from 'react-icons/fa';
-import { useMutation } from '@apollo/client';
-import { DELETE_CLIENT } from '../mutations/clientMutations';
-import { GET_CLIENTS } from '../queries/clientQueries';
+import { FaTrash } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+import { DELETE_CLIENT } from "../mutations/clientMutations";
+import { GET_CLIENTS } from "../queries/clientQueries";
+import Swal from 'sweetalert2';
 
 export const ClientRow = ({ client }) => {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    update(cache, { data: { deleteClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: {
+          clients: clients.filter((client) => client.id !== deleteClient.id),
+        },
+      });
+    },
+  });
 
-    const [deleteClient] = useMutation(DELETE_CLIENT, {
-        variables: { id: client.id },
-        update(cache, { data: { deleteClient } }) {
-            const { clients } = cache.readQuery({ query: GET_CLIENTS });
-            cache.writeQuery({
-                query: GET_CLIENTS,
-                data: { clients: clients.filter(client => client.id !== deleteClient.id) }
-            })
-        }
+  const handleDelete = () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You would not be able to revert this.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Delete",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteClient();
+        Swal.fire("Client Deleted", "The client has been deleted.", "success");
+      }
     });
+  };
 
-
-    return (
-        <tr>
-            <td>{client.name}</td>
-            <td>{client.email}</td>
-            <td>{client.phone}</td>
-            <td>
-                <button className="btn btn-danger btn-sm" onClick={deleteClient}>
-                    <FaTrash />
-                </button>
-            </td>
-        </tr>
-    )
-}
+  return (
+    <tr>
+      <td>{client.name}</td>
+      <td>{client.email}</td>
+      <td>{client.phone}</td>
+      <td>
+        <button className="btn btn-danger btn-sm" onClick={handleDelete}>
+          <FaTrash />
+        </button>
+      </td>
+    </tr>
+  );
+};
